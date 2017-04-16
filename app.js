e4a3f521430ea5db30e2d48f4f3163e50dcb7413
BayMinimum <bearksb1115@gmail.com>
2017-04-16 16:38:20 +0900

    Handle exception when message is not text

--- app.js
+++ app.js
@@ -15,31 +15,37 @@ bot.on('error', (err) => {
 bot.on('message', (payload, reply) => {
     let text = payload.message.text;
     let user_id = payload.sender.id;
-    if(text.indexOf("등록")>=0){
-        db_query(1, user_id, function cb(err, exists) {
-            if(err) reply({text: "오류가 발생했습니다. 다시 시도해 주시겠어요?"}, (err)=>{
-                if(err) console.log(err);
+    try {
+        if (text.indexOf("등록") >= 0) {
+            db_query(1, user_id, function cb(err, exists) {
+                if (err) reply({text: "오류가 발생했습니다. 다시 시도해 주시겠어요?"}, (err) => {
+                    if (err) console.log(err);
+                });
+                else if (exists) reply({text: "이미 등록하셨습니다."}, (err) => {
+                    if (err) console.log(err);
+                });
+                else reply({text: "등록해주셔서 감사합니다. 앞으로 급식/간식 정보를 보내드릴게요!"}, (err) => {
+                        if (err) console.log(err)
+                    });
             });
-            else if(exists) reply({text: "이미 등록하셨습니다."}, (err)=>{
-                if(err) console.log(err);
+        } else if (text.indexOf("해지") >= 0) {
+            db_query(-1, user_id, function cb(err, exists) {
+                if (err) reply({text: "오류가 발생했습니다. 다시 시도해 주시겠어요?"}, (err) => {
+                    if (err) console.log(err);
+                });
+                else if (exists) reply({text: "해지되었습니다. 그동안 이용해 주셔서 감사합니다."}, (err) => {
+                    if (err) console.log(err);
+                });
+                else reply({text: "음...등록하시지 않으셨는데요?"}, (err) => {
+                        if (err) console.log(err)
+                    });
             });
-            else reply({text: "등록해주셔서 감사합니다. 앞으로 급식/간식 정보를 보내드릴게요!"}, (err)=>{
-                if(err) console.log(err)
-            });
-        });
-    }else if(text.indexOf("해지")>=0){
-        db_query(-1, user_id, function cb(err, exists) {
-            if(err) reply({text: "오류가 발생했습니다. 다시 시도해 주시겠어요?"}, (err)=>{
-                if(err) console.log(err);
-            });
-            else if(exists) reply({text: "해지되었습니다. 그동안 이용해 주셔서 감사합니다."}, (err)=>{
-                if(err) console.log(err);
-            });
-            else reply({text: "음...등록하시지 않으셨는데요?"}, (err)=>{
-                if(err) console.log(err)
-            });
-        });
 
+        }
+    }catch (exception){
+        reply({text: "꾸?"}, (err) => {
+            if (err) console.log(err);
+        });
     }
 });
 
